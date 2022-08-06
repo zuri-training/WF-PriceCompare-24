92c213a1cda46cc3cd7aa231141e0b2d17dfe98b
Oputa Olivia Amarachi <72948572+oputaolivia@users.noreply.github.com>
2022-08-06 01:46:40 +0000

    rendered

--- WalletFriendlyRoot/PriceCompare/urls.py
+++ WalletFriendlyRoot/PriceCompare/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
+    path('fashion/', views.fashion, name='fashion.html'),
+    path('fashion/men/', views.fashionmen, name='fashionmen.html'),
+    path('fashion/women/', views.fashionwomen, name='fashionwomen.html'),
+    path('gadgets/', views.gadgets, name='gadget.html'),
+    path('gadget/laptops/', views.laptop, name='laptops.html'),
+    path('gadget/phones/', views.phone, name='phones.html'),
+    path('gadget/tv/', views.tv, name='tv.html'),
+    path('hair/humanhair', views.humanhair, name='humanhair.html'),
+    path('hair/bonestraight/', views.bonestraight, name='bonestraight.html'),
+    path('hair/accessories', views.accessories, name='hairaccessories.html'),
+
+
 ]
