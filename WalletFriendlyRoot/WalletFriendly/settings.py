9524cf2f9d293b274cb47892e1d007f2ad42407b
Charles3K14 <emmacharlesotiji@gmail.com>
2022-08-15 16:01:23 +0100

    code

--- WalletFriendlyRoot/WalletFriendly/settings.py
+++ WalletFriendlyRoot/WalletFriendly/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'walletfriendly/static')]
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
